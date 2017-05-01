7ca67af2bad0f9c9a37e5c259ca61504494a85c9
Austin Wood <austin@austinkwood.com>
2017-04-30 20:52:09 -0700

    Finish Part 1

--- lib/router.rb
+++ lib/router.rb
@@ -16,6 +16,11 @@ class Route
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    match_data = @pattern.match(req.path)
+    route_params = Hash[match_data.names.zip(match_data.captures)]
+
+    controller = @controller_class.new(req, res, route_params)
+    controller.invoke_action(@action_name)
   end
 end
 
@@ -34,21 +39,29 @@ class Router
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
+    self.instance_eval(&proc)
   end
 
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
-    define_method(http_method) do |pattern, method, controller_class, action_name|
-      add_route(pattern, method, controller_class, action_name)
+    define_method(http_method) do |pattern, controller_class, action_name|
+      self.add_route(pattern, http_method, controller_class, action_name)
     end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end
+    nil
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    route = match(req)
+    route ? route.run(req, res) : res.status = 404
+    # TODO: It's also nice to add a message body so the user knows what went wrong.
   end
 end
